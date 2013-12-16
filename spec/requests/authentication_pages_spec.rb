11cc3fe474113f288fbe0b53278e5d808912a743
Barbuto <barbutos92@gmail.com>
2013-12-16 12:59:56 -0500

    chapter 9 w/ errors

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,10 +28,14 @@ describe "AuthenticationPages" do
   end
 
  describe "with valid information" do
-    before { click_button "Sign in" }
-	
-	it { should have_title('Sign in') }
-	it { should have_selector('div.alert.alert-error') } 
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
 		end
 	end
  end   
